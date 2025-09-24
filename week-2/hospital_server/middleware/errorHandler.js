ee2b9fc8a03ea9a936ace67c11ba9fda584178a1
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-24 12:52:19 +0530

    feat: errorHandler.js added in middleware folder

---
+++ week-2/hospital_server/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Something went wrong!';
+  
+    res.status(statusCode).json({
+      message: message,
+      error: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+    });
+  };
+  
+  export default errorHandler;
