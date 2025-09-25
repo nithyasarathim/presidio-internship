06c3b3eef67f4c50a3625ccf595a6b5103667cf9
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-25 20:48:25 +0530

    feat: patientsController.js controller added

---
+++ hospital-api/controllers/patientsController.js
@@ -0,0 +1,76 @@
+const patientService= require("../services/patientServices");
+const logger = require("../utils/logger");
+
+const createPatient = async (req, res, next) => {
+  const apiPath = req.originalUrl;
+  try {
+    logger.log(apiPath, "Create patient request received");
+    const patient = await patientService.createPatient(req.body);
+    logger.log(apiPath, "Patient created successfully");
+    res.status(201).json({ success: true, patient });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getPatients = async (req, res, next) => {
+  const apiPath = req.originalUrl;
+  try {
+    logger.log(apiPath, "Get all patients request received");
+
+    const patients = await patientService.getPatients();
+
+    logger.log(apiPath, "Patients retrieved successfully");
+    res.json({ success: true, patients });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getPatient = async (req, res, next) => {
+  const apiPath = req.originalUrl;
+  try {
+    logger.log(apiPath, `Get patient request received for ID: ${req.params.id}`);
+    const patient = await patientService.getPatientById(req.params.id);
+    logger.log(apiPath, "Patient retrieved successfully");
+    res.json({ success: true, patient });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updatePatient = async (req, res, next) => {
+  const apiPath = req.originalUrl;
+  try {
+    logger.log(apiPath, `Update patient request received for ID: ${req.params.id}`);
+
+    const updatedPatient = await patientService.updatePatient(req.params.id, req.body);
+
+    logger.log(apiPath, "Patient updated successfully");
+    res.json({ success: true, patient: updatedPatient });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deletePatient = async (req, res, next) => {
+  const apiPath = req.originalUrl;
+  try {
+    logger.log(apiPath, `Delete patient request received for ID: ${req.params.id}`);
+
+    await patientService.deletePatient(req.params.id);
+
+    logger.log(apiPath, "Patient deleted successfully");
+    res.json({ success: true, message: "Patient deleted" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports={
+    deletePatient,
+    createPatient,
+    getPatient,
+    getPatients,
+    getPatientById
+}
