d2cc8c8cbf5b6a71bc6747290c9951ee8206a7b1
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-25 22:24:11 +0530

    feat : mailRoutes.js added for email-api

---
+++ email-api/routes/mailRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { sendMailHandler } from "../controllers/mailController.js";
+
+const router = express.Router();
+router.post("/send", sendMailHandler);
+
+export default router;
