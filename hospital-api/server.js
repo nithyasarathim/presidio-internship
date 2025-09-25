c0bb5282790b0f1bd9072daf258c699102d444ab
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-25 20:44:14 +0530

    feat: patientServices.js service added

--- hospital-api/server.js
+++ hospital-api/server.js
@@ -3,11 +3,16 @@ import 'dotenv/config';
 import express from "express";
 import connectDB from "./configs/db.js";
 import requestLogger from "./middlewares/requestLogger.js";
+import errorHandler from "./middlewares/errorHandler.js";
 
 const app=express();
 
 app.use(requestLogger);
 
+
+app.use(errorHandler);
+
 connectDB();
 
-app.listen(5000,()=>{console.log("Server is listening on port 5000")});
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{console.log(`Server is listening on port ${PORT}`)});
---
+++ hospital-api/services/patientServices.js
@@ -0,0 +1,38 @@
+const Patient = require("../models/patient");
+const { sendWelcomeEmail } = require("./emailService");
+const APIError = require("../utils/APIError");
+
+const createPatient = async (data) => {
+  const patient = await Patient.create(data);
+  await sendWelcomeEmail(patient);
+  return patient;
+};
+
+const getPatients = async () => {
+  return await Patient.find();
+};
+
+const getPatientById = async (id) => {
+  const patient = await Patient.findById(id);
+  if (!patient) throw new APIError(404, "Patient not found");
+  return patient;
+};
+
+const updatePatient = async (id, data) => {
+  const patient = await Patient.findByIdAndUpdate(id, data, { new: true });
+  if (!patient) throw new APIError(404, "Patient not found");
+  return patient;
+};
+
+const deletePatient = async (id) => {
+  const patient = await Patient.findByIdAndDelete(id);
+  if (!patient) throw new APIError(404, "Patient not found");
+};
+
+module.exports={
+    createPatient,
+    getPatientById,
+    getPatients,
+    updatePatient,
+    deletePatient
+}
