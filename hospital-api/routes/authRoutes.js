344804b87aeeef5269d111789aeea7e8b22ac198
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-26 20:47:01 +0530

    debug: import error fix - 'middleware'->'middlewares'

--- hospital-api/routes/authRoutes.js
+++ hospital-api/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authController from "../controllers/authController.js";
-import { authenticate } from "../middleware/authMiddleware.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
