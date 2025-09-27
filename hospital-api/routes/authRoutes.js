797b85405dc4bc6e3f8f833619742e1de57dfe60
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-27 11:55:42 +0530

    feature: admin can only register doctors

--- hospital-api/routes/authRoutes.js
+++ hospital-api/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import authController from "../controllers/authController.js";
-import { authenticate } from "../middlewares/authMiddleware.js";
+import { authenticate,authorize} from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", authController.register);
+router.post("/register",authenticate, authorize('admin'), authController.register);
 router.post("/login", authController.login);
 router.get("/profile", authenticate, authController.profile);
 
