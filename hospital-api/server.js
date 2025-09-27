14d4c163b918aa1a0ccce8c0a1c72b303d3cec0b
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-27 12:03:02 +0530

    feat: integrate Swagger UI for API documentation and setup /api/docs route

--- hospital-api/server.js
+++ hospital-api/server.js
@@ -1,4 +1,3 @@
-
 import 'dotenv/config';
 import express from "express";
 import connectDB from "./configs/db.js";
@@ -8,18 +7,47 @@ import authRoutes from "./routes/authRoutes.js";
 import patientsRoutes from "./routes/patientRoutes.js";
 import analyticsRoutes from "./routes/analyticsRoutes.js";
 
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
-const app=express();
+const app = express();
 
 app.use(express.json());
-
 app.use(requestLogger);
-app.use("/api/patients",patientsRoutes);
+
+app.use("/api/patients", patientsRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/analytics",analyticsRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Hospital API",
+      version: "1.0.0",
+      description: "API documentation for Patient, Auth, and Analytics services",
+    },
+    servers: [{ url: "http://localhost:5000/api" }],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  apis: ["./routes/*.js", "./controllers/*.js"], 
+};
+
+const specs = swaggerJsdoc(swaggerOptions);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 app.use(errorHandler);
 
 connectDB();
-
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{console.log(`Server is listening on port ${PORT}`)});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
