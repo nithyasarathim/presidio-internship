5d8ced5a9120171c8616b77588b6e2d82d8b643c
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-27 10:47:46 +0530

    refactor: integrated rate limiter to patientRoutes.js

--- hospital-api/routes/patientRoutes.js
+++ hospital-api/routes/patientRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 import patientController from "../controllers/patientController.js";
 import { authenticate, authorize } from "../middlewares/authMiddleware.js";
+import { patientRateLimiter } from "../middlewares/rateLimitMiddleware.js";
 
-router.post("/", authenticate, authorize("doctor"), patientController.createPatient);
-router.get("/", authenticate, authorize("doctor"), patientController.getPatients);
-router.get("/:id", authenticate, authorize("doctor"), patientController.getPatient);
-router.put("/:id", authenticate, authorize("doctor"), patientController.updatePatient);
-router.delete("/:id", authenticate, authorize("doctor"), patientController.deletePatient);
+router.post("/", authenticate, authorize("doctor"), patientRateLimiter, patientController.createPatient);
+router.get("/", authenticate, authorize("doctor"), patientRateLimiter, patientController.getPatients);
+router.get("/:id", authenticate, authorize("doctor"), patientRateLimiter, patientController.getPatient);
+router.put("/:id", authenticate, authorize("doctor"), patientRateLimiter, patientController.updatePatient);
+router.delete("/:id", authenticate, authorize("doctor"), patientRateLimiter, patientController.deletePatient);
 
 export default router;
