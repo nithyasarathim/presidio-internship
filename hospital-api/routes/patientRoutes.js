384c1e349a81c9c74156ab40a17be2bd84721631
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-09-27 11:25:49 +0530

    rename: 'patientRateLimiter' to 'doctorRateLimiter'

--- hospital-api/routes/patientRoutes.js
+++ hospital-api/routes/patientRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 import patientController from "../controllers/patientController.js";
 import { authenticate, authorize } from "../middlewares/authMiddleware.js";
-import { patientRateLimiter } from "../middlewares/rateLimitMiddleware.js";
+import { doctorRateLimiter } from "../middlewares/rateLimitMiddleware.js";
 
-router.post("/", authenticate, authorize("doctor"), patientRateLimiter, patientController.createPatient);
-router.get("/", authenticate, authorize("doctor"), patientRateLimiter, patientController.getPatients);
-router.get("/:id", authenticate, authorize("doctor"), patientRateLimiter, patientController.getPatient);
-router.put("/:id", authenticate, authorize("doctor"), patientRateLimiter, patientController.updatePatient);
-router.delete("/:id", authenticate, authorize("doctor"), patientRateLimiter, patientController.deletePatient);
+router.post("/", authenticate, authorize("doctor"), doctorRateLimiter, patientController.createPatient);
+router.get("/", authenticate, authorize("doctor"), doctorRateLimiter, patientController.getPatients);
+router.get("/:id", authenticate, authorize("doctor"), doctorRateLimiter, patientController.getPatient);
+router.put("/:id", authenticate, authorize("doctor"), doctorRateLimiter, patientController.updatePatient);
+router.delete("/:id", authenticate, authorize("doctor"), doctorRateLimiter, patientController.deletePatient);
 
 export default router;
