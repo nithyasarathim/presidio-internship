420a0397210c96d0441128bf3b97680f480fb54b
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-10-05 20:06:17 +0530

    refactor: ProtectedRoute.jsx

--- hospital-client/src/auth/ProtectedRoute.jsx
+++ hospital-client/src/auth/ProtectedRoute.jsx
@@ -3,22 +3,27 @@ import { Navigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import Error403 from "../error/Error403";
 
-const ProtectedRoute = ({ children, allowedRole = "" }) => {
+const ProtectedRoute = ({ children,allowedRole}) => {
+
   const token = localStorage.getItem("token");
+  const [role,setRole]=useState("");
+
   if (!token) {
     return <Navigate to="/login" replace />;
   }
-  let role = "";
+
   try {
     const decoded = jwt_decode(token);
-    const {role}=decoded;
+    setRole(decoded.role);
   } catch (err) {
     console.error("Invalid token:", err);
     return <Navigate to="/login" replace />;
   }
-  if (allowedRole && role !== allowedRole) {
+
+  if (allowedRole && role!==allowedRole) {
     return <Error403 />;
   }
+  
   return children;
 };
 
