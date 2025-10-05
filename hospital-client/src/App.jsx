fe169385f36836eb4dc106814e321d821e22636d
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-10-05 20:12:45 +0530

    refactor: App.js added with routes

--- hospital-client/src/App.jsx
+++ hospital-client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route, Navigate} from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
 import DoctorPage from './pages/DoctorPage';
 import LoginPage from './pages/LoginPage';
@@ -12,7 +12,18 @@ const App = () => {
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
-        <Ro
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/admin' element={
+            <ProtectedRoute allowedRole={"admin"}>
+              <AdminPage/>
+            </ProtectedRoute>
+        }/>
+        <Route path='/doctor' element={
+          <ProtectedRoute allowedRole={"doctor"}>
+            <DoctorPage/>
+          </ProtectedRoute>
+        }/>
+        <Route path='*' element={<Navigate to="/" replace/>}/>
       </Routes>
     </Router>
   )
