ee3d2cd8b43c3781125edfd257d6496abd950ac5
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-10-06 17:39:27 +0530

    update:patient modal updated

--- hospital-api/modals/patient.js
+++ hospital-api/modals/patient.js
@@ -1,31 +1,39 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const patientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    age: {
+      type: Number,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    phone: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    dob: {
+      type: Date,
+      required: true
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true
+    }
   },
-  age: {
-    type: Number,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  dob: {
-    type: Date,
-    required: true
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Patient = mongoose.model("Patient", patientSchema);
 
