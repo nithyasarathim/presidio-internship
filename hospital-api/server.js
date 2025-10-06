c5302d770a1565f837c1a73c242cab69d9aeff8e
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-10-06 09:45:39 +0530

    debug: cors issue

--- hospital-api/server.js
+++ hospital-api/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(requestLogger);
-app.use(cors('*'));
+app.use(cors());
 
 app.use("/api/patients", patientsRoutes);
 app.use("/api/auth", authRoutes);
