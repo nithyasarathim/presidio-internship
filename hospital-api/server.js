4ec9ce356d11e1079b4b66bacbf3c2a4d2dcc6e2
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-10-06 09:44:40 +0530

    update: cors allowed from anywhere

--- hospital-api/server.js
+++ hospital-api/server.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import 'cors';
 import express from "express";
 import connectDB from "./configs/db.js";
 import requestLogger from "./middlewares/requestLogger.js";
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors('*'));
 
 app.use("/api/patients", patientsRoutes);
 app.use("/api/auth", authRoutes);
