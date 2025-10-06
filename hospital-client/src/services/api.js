03170d24c06aa7d51773f4af692ede5919584346
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-10-06 09:04:22 +0530

    feat: service layer created for API calls

---
+++ hospital-client/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+const baseURL=import.meta.env.VITE_BASE_URL;
+
+const useApi=()=>{
+  const {token}=useContext(UserContext); 
+  const api=axios.create({
+    baseURL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": "application/json",
+    },
+  });
+  return api;
+};
+
+export default useApi;
